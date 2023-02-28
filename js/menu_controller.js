563dcadb4fea59c8d7a518f0819f710a3bc6b56b
JoanSF99 <al376128@uji.es>
2023-02-28 13:34:01 +0100

    Update menu_controller.js

--- js/menu_controller.js
+++ js/menu_controller.js
@@ -2,16 +2,4 @@ function start_game(){
 	loadpage("./pi_2/index.html");
 }
 
-function exit (){
-	if (name != ""){
-		alert("Leaving " + name + "'s game");
-	}
-	name = "";
-}
-
-function options(){
-	// TODO: Open options menu
-	console.log("Options menu button");
-}
-
 
