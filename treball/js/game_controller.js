e236190cd639a62c8bc691a57613442ae6a20d1e
JoanSF99 <al376128@uji.es>
2023-03-27 17:55:20 +0200

    cartes al cancas

--- treball/js/game_controller.js
+++ treball/js/game_controller.js
@@ -12,17 +12,28 @@ var config = {
 var game = new Phaser.Game(config);
 
 function preload() {
-    this.load.image("back", "../resources/back.png")
-	this.load.image("cb", "../resources/cb.png")
-	this.load.image("co", "../resources/co.png")
-	this.load.image("sb", "../resources/sb.png")
-	this.load.image("so", "../resources/so.png")
-	this.load.image("tb", "../resources/tb.png")
-	this.load.image("to", "../resources/to.png")
+    this.load.image("back", "../resources/back.png");
+	this.load.image("cb", "../resources/cb.png");
+	this.load.image("co", "../resources/co.png");
+	this.load.image("sb", "../resources/sb.png");
+	this.load.image("so", "../resources/so.png");
+	this.load.image("tb", "../resources/tb.png");
+	this.load.image("to", "../resources/to.png");
 }
 
 function create() {
-    // Creación de objetos del juego
+	this.cameras.main.setBackgroundColor(0xBFFCFF);
+
+	var xPos = 250;
+	var yPos = 300;
+
+	this.add.image(xPos, yPos, "co");
+	xPos += 100;
+	this.add.image(xPos, yPos, "sb");
+	xPos += 100;
+	this.add.image(xPos, yPos, "co");
+	xPos += 100;
+	this.add.image(xPos, yPos, "sb");
 }
 
 function update() {
