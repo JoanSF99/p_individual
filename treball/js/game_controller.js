9720a5cdf017a1258c4b5658bfe95bdc1f0b26c4
JoanSF99 <al376128@uji.es>
2023-03-27 17:22:07 +0200

    Crear phaser i preload

--- treball/js/game_controller.js
+++ treball/js/game_controller.js
@@ -1,96 +1,30 @@
-const back = "../resources/back.png";
-const items = ["../resources/cb.png","../resources/co.png","../resources/sb.png",
-"../resources/so.png","../resources/tb.png","../resources/to.png"];
+var config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
 
-var game = new Vue({
-	el: "#game_id",
-	data: {
-		username:'',
-		current_card: [],
-		items: [],
-		num_cards: 2,
-		bad_clicks: 0,
-		gameStarted: false,
-		penalty:0
-	},
-	created: function(){
-		var json = localStorage.getItem("config") || '{"cards":2,"dificulty":"hard"}';
-		options_data = JSON.parse(json);
-		this.dificulty = options_data.dificulty;
-		this.username = sessionStorage.getItem("username","unknown");
-		this.items = items.slice(); // Copy the array
-		this.items.sort(function(){return Math.random() - 0.5}); // Shuffle the array
-		this.num_cards = options_data.cards; // Set num_cards to the value from localStorage
-		this.items = this.items.slice(0, this.num_cards); // Take the first numCards elements
-		this.items = this.items.concat(this.items); // Duplicate the elements
-		this.items.sort(function(){return Math.random() - 0.5}); // Shuffle the array
-		for (var i = 0; i < this.num_cards * 2; i++) {
-				this.current_card.push({done: false, texture: this.items[i]});
-		}
+var game = new Phaser.Game(config);
 
-		if(this.dificulty=="hard"){
-			Vue.set(this, 'penalty', 25)
-			setTimeout(() => {
-				this.turnCards();
-			}, 1000);
-		}
-		else if(this.dificulty=="normal"){
-			Vue.set(this, 'penalty', 20)
-			setTimeout(() => {
-				this.turnCards();
-			}, 2000);
-		}
-		else{
-			Vue.set(this, 'penalty', 10)
-			setTimeout(() => {
-				this.turnCards();
-			}, 3000);
-		}
-		
-	},
-	methods: {
-		clickCard: function(i){
-			if (!this.current_card[i].done && this.current_card[i].texture === back)
-				Vue.set(this.current_card, i, {done: false, texture: this.items[i]});
-		},
-		turnCards: function(){
-			Vue.set(this, 'gameStarted', true);
-			for (var i = 0; i < this.current_card.length; i++){
-				Vue.set(this.current_card, i, {done: false, texture: back});
-			}	
-		}
-	},
-	watch: {
-		current_card: function(value){
-			if (!this.gameStarted) return;
-			if (value.texture === back) return;
-			var front = null;
-			var i_front = -1;
-			for (var i = 0; i < this.current_card.length; i++){
-				if (!this.current_card[i].done && this.current_card[i].texture !== back){
-					if (front){
-						if (front.texture === this.current_card[i].texture){
-							front.done = this.current_card[i].done = true;
-							this.num_cards--;
-						}
-						else{
-							Vue.set(this.current_card, i, {done: false, texture: back});
-							Vue.set(this.current_card, i_front, {done: false, texture: back});
-							this.bad_clicks++;
-							break;
-						}
-					}
-					else{
-						front = this.current_card[i];
-						i_front = i;
-					}
-				}
-			}			
-		}
-	},
-	computed: {
-		score_text: function(){
-			return 100 - this.bad_clicks * this.penalty;
-		}
-	}
-});
+function preload() {
+    this.load.image("back", "../resources/back.png")
+	this.load.image("cb", "../resources/cb.png")
+	this.load.image("co", "../resources/co.png")
+	this.load.image("sb", "../resources/sb.png")
+	this.load.image("so", "../resources/so.png")
+	this.load.image("tb", "../resources/tb.png")
+	this.load.image("to", "../resources/to.png")
+}
+
+function create() {
+    // Creación de objetos del juego
+}
+
+function update() {
+    // Actualización del juego
+}
