cad51c6f9943e53d04b5acf4878c630d842b4b87
JoanSF99 <al376128@uji.es>
2023-03-27 17:09:54 +0200

    Crear treball i botó phaser

--- js/menu_controller.js
+++ js/menu_controller.js
@@ -2,4 +2,8 @@ function start_game(){
 	loadpage("./pi_3/index.html");
 }
 
+function start_game_phaser(){
+	loadpage("./treball/index.html");
+}
+
 
---
+++ treball/js/menu_controller.js
@@ -0,0 +1,21 @@
+function start_game(){
+	name = prompt("User name");
+	
+	sessionStorage.setItem("username", name);
+	
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	if (name != ""){
+		alert("Leaving " + name + "'s game");
+	}
+	name = "";
+	loadpage("../index.html");
+}
+
+function options(){
+	loadpage("./html/options.html");
+}
+
+
