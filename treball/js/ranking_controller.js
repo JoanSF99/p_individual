92ca74c975d44616c90073e6a34cb0be75384a92
JoanSF99 <78982038+JoanSF99@users.noreply.github.com>
2023-05-03 11:11:53 +0200

    update

--- treball/js/ranking_controller.js
+++ treball/js/ranking_controller.js
@@ -1,21 +1,21 @@
 new Vue({
-    el: '#ranking_id',
-    data: {
-      topScores: []
-    },
-    mounted() {
-        console.log(localStorage.getItem('scores'))
-
-      if (localStorage.getItem('scores')) {
-        try {
-          let json = localStorage.getItem('scores');
-          var sacores = JSON.parse(json);
-          console.log(this.scores)
-          this.topScores = scores.slice(0, 5);
-        } catch(e) {
-          localStorage.removeItem('scores');
-        }
+  el: '#ranking_id',
+  data: {
+    topScores: []
+  },
+  created() {
+    console.log(localStorage.getItem('scores'))
+    if (localStorage.getItem('scores')) {
+      try {
+        let json = localStorage.getItem('scores');
+        console.log(json)
+        var scores = JSON.parse(json);
+        console.log(this.scores)
+        this.topScores = scores.slice(0, 5);
+        console.log(this.topScores)
+      } catch(e) {
+        localStorage.removeItem('scores');
       }
-      console.log(this.topScores)
     }
-  })
+  }
+})
